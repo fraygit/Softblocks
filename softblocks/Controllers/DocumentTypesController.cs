fc2f1ef857048a769c88bbd5dafcc57912dcbc0b
fray <francis.yanga@gmail.com>
2017-06-23 09:35:41 +1200

    Create document type ui

---
+++ softblocks/Controllers/DocumentTypesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace softblocks.Controllers
+{
+    public class DocumentTypesController : Controller
+    {
+        // GET: DocumentTypes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
