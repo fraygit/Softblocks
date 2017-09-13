960550d99533ff7e27f350c1a6a0b092b11512af
fray <francis.yanga@gmail.com>
2017-09-14 10:16:06 +1200

    module details

---
+++ softblocks/assets/js/common.js
@@ -0,0 +1,36 @@
+﻿function AddLoadingOverlay(elementName) {
+    $(elementName).block({
+        message: '<h4><img src="/plugins/images/busy.gif" /> Just a moment...</h4>',
+        css: {
+            border: '1px solid #fff'
+        }
+    });
+}
+
+function RemoveLoadingOverlay(elementName) {
+    $(elementName).unblock();
+}
+
+function alertSuccess(title, message) {
+    $.toast({
+        heading: title,
+        text: message,
+        position: 'top-right',
+        loaderBg: '#ff6849',
+        icon: 'success',
+        hideAfter: 2000,
+        stack: 6
+    });
+}
+
+function alertError(title, message) {
+    $.toast({
+        heading: title,
+        text: message,
+        position: 'top-right',
+        loaderBg: '#ff6849',
+        icon: 'success',
+        hideAfter: 2000,
+        stack: 6
+    });
+}
