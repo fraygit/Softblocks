7c3b9f6f9b76c412a6e9b4bfe415383eab23ba96
fray <francis.yanga@gmail.com>
2017-09-23 01:30:51 +1200

    Data View init

---
+++ softblocks.data/Model/DataView.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace softblocks.data.Model
+{
+    public class DataView
+    {
+        public ObjectId Id { get; set; }
+        public String Name { get; set; }
+        public string Description { get; set; }
+        public string DataViewType { get; set; }
+        public List<DataViewColumns> Columns { get; set; }
+    }
+}
---
+++ softblocks.data/Model/DataViewColumns.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace softblocks.data.Model
+{
+    public class DataViewColumns
+    {
+        public ObjectId FieldId { get; set; }
+        public int Order { get; set; }
+    }
+}
