8483cb39f7f3f3950ce39a6027f177ffbae518a5
fray <francis.yanga@gmail.com>
2017-09-28 12:11:33 +1300

    render detail data view

--- softblocks/Controllers/DataViewController.cs
+++ softblocks/Controllers/DataViewController.cs
@@ -92,6 +92,49 @@ namespace softblocks.Controllers
         }
 
         [Authorize]
+        public async Task<ActionResult> RenderDetail(string appId, string id)
+        {
+            if (!string.IsNullOrEmpty(appId))
+            {
+                var response = new ResRenderTabular();
+                var appModule = await _appModuleRepository.Get(appId);
+                if (appModule != null)
+                {
+                    if (appModule.Forms != null)
+                    {
+                        ViewBag.AppId = appModule.Id.ToString();
+                        ViewBag.AppName = appModule.Name;
+
+                        ObjectId dataViewId;
+                        if (ObjectId.TryParse(id, out dataViewId))
+                        {
+                            if (appModule.DataViews.Any(n => n.Id == dataViewId))
+                            {
+                                var dataView = appModule.DataViews.FirstOrDefault(n => n.Id == dataViewId);
+                                response.DataView = dataView;
+                                var docTypeService = new DocumentTypeServices(_appModuleRepository);
+                                response.DocumentFields = await docTypeService.FindDocumentType(appId, dataView.DocumentTypeId);
+
+                                //var org = await _organisationRepository.Get(appModule.OrganisationId);
+
+                                //var dataService = new DataService(ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString, org.Id.ToString(), appModule.Name);
+                                //var data = await dataService.ListAll();
+                                //var jsonWriterSettings = new JsonWriterSettings { OutputMode = JsonOutputMode.Strict };
+                                //response.Data = data.ToJson(jsonWriterSettings);
+
+                                return View(response);
+                            }
+                        }
+
+                        return View(response);
+                    }
+                    return View(new DataView());
+                }
+            }
+            return View();
+        }
+
+        [Authorize]
         public async Task<ActionResult> RenderTabular(string appId, string id)
         {
             if (!string.IsNullOrEmpty(appId))
