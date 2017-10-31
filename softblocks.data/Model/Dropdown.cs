ee231d43f9737e27f5303e448b0cf46c2a3f47f5
fray <francis.yanga@gmail.com>
2017-10-31 22:29:00 +1300

    dropdown data

---
+++ softblocks.data/Model/Dropdown.cs
@@ -0,0 +1,16 @@
+﻿using softblocks.data.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace softblocks.data.Model
+{
+    public class Dropdown : MongoEntity
+    {
+        public string Name { get; set; }
+        public string AppModuleId { get; set; }
+        public List<string> Items { get; set; }
+    }
+}
