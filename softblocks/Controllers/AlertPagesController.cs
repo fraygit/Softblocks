1b96cce73298eb1b1db385f6144468695db383a6
fray <francis.yanga@gmail.com>
2017-11-21 11:59:18 +1300

    clean up pages

---
+++ softblocks/Controllers/AlertPagesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace softblocks.Controllers
+{
+    public class AlertPagesController : Controller
+    {
+        // GET: AlertPages
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult NotAvailable()
+        {
+            return View();
+        }
+    }
+}
